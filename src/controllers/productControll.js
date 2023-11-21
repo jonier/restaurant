9489c7764e4406d7704e88dbf904ef5cb4dc5f68
Jonier <jonierm@gmail.com>
2023-11-20 23:47:26 -0500

    productRouter and productController updated

--- src/controllers/productControll.js
+++ src/controllers/productControll.js
@@ -1,7 +1,27 @@
 const Product = require("../models/product");
 
-const getAllProducts = (req, res) => {
-    res.send("<h1>Producto desde Controller</h1>")
+const getAllProducts = (req, res, next) => {
+    Product.findAll()
+    .then(products => {
+        res.status(201).send({ status: "OK", data: products });
+    })
+    .catch(error => {
+        console.log("Error: ", error);
+    })
+    
+}
+
+const getProductByPk = (req, res, next) => {
+    const productId = req.params.productId;
+    Product.findByPk(productId)
+    .then(product => {
+        const { row } = result;
+        console.log('Resultado', row);
+        res.status(201).send({status: "OK", data: product});
+    })
+    .catch(error => {
+        console.log("Error: ", error);
+    })
 }
 
 const postAddProduct = (req, res, next) => {
@@ -28,7 +48,51 @@ const postAddProduct = (req, res, next) => {
 
 }
 
+const postEditProduct = (req, res, next) => {
+    const productId = req.body.id;
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    Product.findByPk(productId)
+    .then(product => {        
+        product.title = title;
+        product.imageUrl = imageUrl;
+        product.price = price;
+        product.description = description;
+        product.updatedAt = new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+        product.save()
+    })
+    .then(result => {
+        console.log('Super resultado: ', result)
+        res.status(201).send({ status: "OK" });
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+}
+
+const deleteProductById = (req, res, next) => {
+    const productId = req.params.productId;
+
+    Product.findByPk(productId)
+    .then(product => {
+        return product.destroy();
+    })
+    .then(result => {
+        res.status(201).send({ status: "Product destroyed", data: result });
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
 module.exports = {
     getAllProducts,
-    postAddProduct
+    postAddProduct,
+    getProductByPk,
+    postEditProduct,
+    deleteProductById
 }
--- src/v1/routes/productRoutes.js
+++ src/v1/routes/productRoutes.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 router
     .get("/", productController.getAllProducts)
+    .get("/:productId", productController.getProductByPk)
     .post("/", productController.postAddProduct)
+    .patch("/", productController.postEditProduct)
+    .delete("/:productId", productController.deleteProductById)
 
 module.exports = router;
